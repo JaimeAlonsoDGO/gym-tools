97ea09eaaa0762e591a12ddf48ff7c780781f1b5
Jaime Alonso <jaimealonsomail@gmail.com>
2024-02-16 14:53:47 -0600

    upgrade(PTFM): App dropdown and items

---
+++ components/atoms/dropdowns/AppDropdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="relative" @mouseenter="show = true" @mouseleave="show = false">
+    <slot />
+    <div
+      v-show="show"
+      class="absolute top-full right-0 bg-white p-[8px] border border-gray-100"
+    >
+      <slot name="dropdown">
+        <AppDropdownItem v-for="item in items" :key="item.name" v-bind="item" />
+      </slot>
+    </div>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  import AppDropdownItem from '~/components/atoms/dropdowns/AppDropdownItem.vue';
+
+  const props = defineProps({
+    items: {
+      type: Array,
+      default: () => [],
+      /* {
+        text: {
+          type: String,
+          default: '',
+        },
+        icon: {
+          type: [Object, null],
+          default: null,
+        },
+        onClick: {
+          type: Function,
+          default: () => {},
+        },
+      }
+    */
+    },
+  });
+
+  const show = ref(false);
+</script>
---
+++ components/atoms/dropdowns/AppDropdownItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <slot>
+      <AppButton
+        class="text-left whitespace-nowrap w-full"
+        :theme="theme"
+        :size="size"
+        @onClick="onClick"
+      >
+        <AppIcon v-if="icon" v-bind="icon" class="w-[16px] h-[16px]" />
+        {{ text }}
+      </AppButton>
+    </slot>
+  </div>
+</template>
+<script setup>
+  import AppButton from '~/components/atoms/buttons/AppButton.vue';
+  import AppIcon from '~/components/atoms/icons/AppIcon.vue';
+
+  const props = defineProps({
+    theme: {
+      type: String,
+      default: 'primary',
+    },
+    size: {
+      type: String,
+      default: 'medium',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: [Object, null],
+      default: null,
+    },
+    onClick: {
+      type: Function,
+      default: () => {},
+    },
+  });
+</script>
