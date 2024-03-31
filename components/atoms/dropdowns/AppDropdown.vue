b3843d8108dc2ac131d6a75ba822930eb94de6d7
Jaime Alonso <jaimealonsomail@gmail.com>
2024-03-31 14:17:59 -0600

    upgrade(PTFM): App dropdown with click outside directive

--- components/atoms/dropdowns/AppDropdown.vue
+++ components/atoms/dropdowns/AppDropdown.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="relative" @mouseenter="show = true" @mouseleave="show = false">
+  <div
+    ref="app-dropdown"
+    class="relative"
+    v-click-outside="() => (show = false)"
+    @click="onChangeClick"
+    @mouseenter="onChangeHover(true)"
+    @mouseleave="onChangeHover(false)"
+  >
     <slot />
     <div
       v-show="show"
-      class="absolute top-full right-0 bg-white dark:bg-background-light p-[8px] border border-gray-100 dark:border-gray-500"
+      class="absolute z-10 top-full right-0 bg-white dark:bg-background-light p-[8px] border border-gray-100 dark:border-gray-500"
     >
       <slot name="dropdown">
         <AppDropdownItem v-for="item in items" :key="item.name" v-bind="item" />
@@ -35,7 +42,25 @@
       }
     */
     },
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
   });
 
   const show = ref(false);
+
+  const onChangeHover = (value) => {
+    if (props.clickable) {
+      return;
+    }
+    show.value = value;
+  };
+
+  const onChangeClick = () => {
+    if (!props.clickable) {
+      return;
+    }
+    show.value = !show.value;
+  };
 </script>
